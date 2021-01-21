e4fcd829597101672c8019be0d3249d38e6b3566
zhengchao <chao.zheng@okcoin.com>
2021-01-21 16:28:20 +0800

    okchain->okexchain

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='okchain1-docs-theme',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'Sphinx==1.7.4',
+#           'Sphinx==1.7.4',
           'sphinxcontrib-plantuml==0.14',
           'sphinx_sitemap==0.3.1',
       ],
